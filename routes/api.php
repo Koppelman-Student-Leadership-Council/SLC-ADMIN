fb4b3cd4377c62cce117e2b0506ce746c05b8e76
NelsonWang <wangnelson2@gmail.com>
2023-10-04 17:15:27 -0400

    api

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,10 @@ use App\Http\Controllers\ApiController;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-
-    
     return $request->user();
 });
+
+Route::get('/', [ApiController::class, 'index']);
 Route::get('/events', [ApiController::class, 'getAllEvents']);
 Route::get('/team', [ApiController::class, 'getAllTeam']);
 Route::get('/clubs', [ApiController::class, 'getAllClubs']);
