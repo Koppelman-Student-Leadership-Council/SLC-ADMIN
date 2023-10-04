3ddf0f7b24d8551ac33b33c7cce0d18ad669b160
NelsonWang <wangnelson2@gmail.com>
2023-10-04 15:29:01 -0400

    api | calendar

--- routes/api.php
+++ routes/api.php
@@ -31,21 +31,5 @@ Route::get('/events-calendar/title/{title}', [ApiController::class, 'getGoogleIn
 Route::get('/team/department/{department}', [ApiController::class, 'getTeamFromDepartmentActive']);
 
 Route::get('/events/title/{title}', [ApiController::class, 'getIndividualEvent']);
-<<<<<<< HEAD
-Route::get('/events-calendar-non', [ApiController::class, 'getAllEvents']);
-Route::get('/events-calendar-non/{title}', [ApiController::class, 'existsInDatabaseRes']);
-
-
-
-
-
-
-
-
-
-
-
-=======
 Route::get('/events-calendar-non', [ApiController::class, 'getGoogleEventsNotInDatabase']);
 Route::get('/events-calendar-non/{title}', [ApiController::class, 'existsInDatabaseRes']);
->>>>>>> master
